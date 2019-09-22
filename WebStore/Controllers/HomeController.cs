5db984fe4d4c0230bfae7a777dd8195b386eb5a7
Havok <havokrazor718@gmail.com>
2019-09-22 18:06:14 +0300

    Finished Lesson_2

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -12,8 +12,39 @@ namespace WebStore.Controllers
         {
             return View();
         }
-
         public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult BlogSingle()
+        {
+            return View();
+        }
+        public IActionResult FourOFour()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult ProductDetails()
+        {
+            return View();
+        }
+        public IActionResult Shop()
         {
             return View();
         }
